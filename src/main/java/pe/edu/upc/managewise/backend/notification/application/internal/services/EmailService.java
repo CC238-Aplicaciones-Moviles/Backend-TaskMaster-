177a0cd77e1ff1a240089273f756c31fc1a45c42
loreleysarmiento <sarmientoloreley@gmail.com>
2025-12-05 03:47:52 -0500

    Notificaciones por Gmail

---
+++ src/main/java/pe/edu/upc/managewise/backend/notification/application/internal/services/EmailService.java
@@ -0,0 +1,30 @@
+package pe.edu.upc.managewise.backend.notification.application.internal.services;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.mail.from}")
+    private String from;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendNotificationEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+}
